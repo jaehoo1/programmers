6808fea8ae665129d12170133d917d34cbab6ca4
Jeon Jae Ho <jaehoo8936@naver.com>
2022-02-06 13:13:01 +0900

    Create 12925 문자열을 정수로 바꾸기.c

---
+++ programmers/12925 문자열을 정수로 바꾸기.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* s) {
+    int answer = 0;
+    sscanf(s, "%d", &answer);
+    return answer;
+}

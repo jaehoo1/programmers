8b5b1b5677a9611bfaf185c1aac447255804008f
Jeon Jae Ho <jaehoo8936@naver.com>
2022-02-19 10:46:37 +0900

    Create 59409 중성화 여부 파악하기.sql

---
+++ programmers/59409 중성화 여부 파악하기.sql
@@ -0,0 +1,6 @@
+select ANIMAL_ID, NAME, case
+    when SEX_UPON_INTAKE like '%Neutered%' then 'O'
+    when SEX_UPON_INTAKE like '%Spayed%' then 'O'
+    else 'X'
+    end
+as 중성화 from ANIMAL_INS order by ANIMAL_ID;
